e674e3a3bd8bf3ce451a02acae72e703faa191c1
tjdahr7638 <tjdahr7638@gmail.com>
2017-03-05 22:19:33 +0900

    master branch change a code

--- Hello world/src/test/HelloWorld.java
+++ Hello world/src/test/HelloWorld.java
@@ -6,6 +6,7 @@ public class HelloWorld {
 		// TODO Auto-generated method stub
 		System.out.println("Hello world");
 		System.out.println("World's End Dancehall");
+		System.out.println("Earth's End Dancehall");
 
 	}
 
